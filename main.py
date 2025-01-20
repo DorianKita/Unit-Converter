07eb24713e58d2ad4fc4c6c8674ec7a1d14295d3
Dorian Kita <dorian.kita90@gmail.com>
2025-01-20 21:56:57 +0100

    new placing options

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 import tkinter
 
-window = tkinter.Tk()
-
-window.title("My first GUI program")
-window.minsize(width=500, height=300)
-
-#labels
-label = tkinter.Label(text="I'm a Label", font=("Arial", 24, "bold"))
-label.pack()
-
-label.config(text="This is a new label")
-
-#Button
 def button_clicked():
     username = input.get()
     label.config(text=username)
 
+window = tkinter.Tk()
+window.title("My first GUI program")
+window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
+
+#labels
+label = tkinter.Label(text="I'm a Label", font=("Arial", 24, "bold"))
+label.config(text="This is a new label")
+label.grid(row= 0, column= 0)
+
+#Button
 button = tkinter.Button(text="Click me", command=button_clicked)
-button.pack()
+new_button = tkinter.Button(text="New Button")
+button.grid(row=1, column=1)
+new_button.grid(row=0, column=2)
 
 #Entry/input
-
 input = tkinter.Entry(width=10)
-input.pack()
-
+input.grid(row=2,column=3)
 
 
 window.mainloop()
