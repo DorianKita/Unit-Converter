5667eb5661df91c9b530cf5f1c429420bd7debbd
Dorian Kita <dorian.kita90@gmail.com>
2025-01-20 20:47:15 +0100

    tried *kwargs method

--- playground.py
+++ playground.py
@@ -6,3 +6,14 @@ def add(*args):
     print(total)
 
 add(5,4,11,15)
+
+#kwargs
+
+def calculate(n, **kwargs):
+
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+
+    print(n)
+
+calculate(2,add=3, multiply=5)
