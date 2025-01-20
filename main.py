ea6dca2747047a39d8eaf9431209e2916017cbcc
Dorian Kita <dorian.kita90@gmail.com>
2025-01-20 21:22:42 +0100

    Had fun with basic building blocks for tkinter

--- main.py
+++ main.py
@@ -9,5 +9,21 @@ window.minsize(width=500, height=300)
 label = tkinter.Label(text="I'm a Label", font=("Arial", 24, "bold"))
 label.pack()
 
+label.config(text="This is a new label")
+
+#Button
+def button_clicked():
+    username = input.get()
+    label.config(text=username)
+
+button = tkinter.Button(text="Click me", command=button_clicked)
+button.pack()
+
+#Entry/input
+
+input = tkinter.Entry(width=10)
+input.pack()
+
+
 
 window.mainloop()
